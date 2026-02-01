c1e31774ad4216ebfdc57ac2133209d1c6a3bbab
Dipali <dipalich2004@gmail.com>
2026-02-01 12:24:11 +0530

    refactor 05_objects2.js to clean up commented code and add course instructor logging

--- 05_objects2.js
+++ 05_objects2.js
@@ -16,8 +16,18 @@ const regularUser={
 const target={a:1,b:2};
 const source={b:4,c:5};
 const returnedTarget=Object.assign(source,target);
-console.log(source);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("name"));
+// console.log(source);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("name"));
+const course={
+    coursename:"Javascript",
+    price:999,
+    courseInstructor:"Dipali Chavan"
+}
+const{courseInstructor:instructor}=course;
+console.log(instructor);
+// for(let key in course){
+//     //console.log(`${key} : ${course[key]}`);
+//
