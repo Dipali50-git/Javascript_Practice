3b6e3c7388464db3d02b999c2b923ab0d55123b2
Dipali <dipalich2004@gmail.com>
2026-02-02 21:19:32 +0530

    add 06_function.js with examples of function creation and user login messages in JavaScript

---
+++ 06_function.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(Number1, Number2){
+    console.log(Number1+Number2);
+}
+const result=addTwoNumbers(5,10);
+console.log(result);
+function loginUserMessage(username){
+    return`${username} just logged in`;
+}
+console.log(loginUserMessage("Dipali"));
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter a valid username");
+        return;
+}
+return `${username} just logged in`
+}
+console.log(loginUserMessage("Dipali"))
